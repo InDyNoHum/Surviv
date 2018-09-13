3292fde3adc2993190a9c65863b59ea805e743ab
InDyNoHum <42632241+InDyNoHum@users.noreply.github.com>
2018-09-13 14:22:18 +0700

    SV

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1536822207,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tilemap.png",
+		"images/tilemap3.png",
+		"images/hero-sheet0.png",
+		"images/demon-sheet0.png",
+		"images/moneys-sheet0.png",
+		"images/lava-sheet0.png",
+		"images/money-sheet0.png",
+		"images/boss-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
